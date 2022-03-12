f317956fffea5d981fd8ec7567b48e7d8fdd33a1
CatCalledFelix <96912106+CatCalledFelix@users.noreply.github.com>
2022-03-11 21:56:34 -0500

    Add files via upload

---
+++ test-python-0x00.py
@@ -0,0 +1,49 @@
+#!/bin/python
+
+#ping python script
+
+from icmplib import ping
+
+#all hosts
+a_hosts = []
+
+# main starting function
+def start_page():
+    vchoos = input("Choose an option\n1.See all Hosts\n2.Add new Host\n3.Start script\n")
+    if vchoos == "1":
+        p_hosts()
+        start_page()
+    elif vchoos == "2":
+        a_host()
+    elif vchoos == "3":
+        st_script()
+    else:
+        print("Not an option, try again")
+        start_page()
+
+# adding hosts
+def a_host():
+    ad_host = input("please enter new hosts ip/domain or B to back to main list:\n")
+    if ad_host == "B":
+        start_page()
+    else:
+        a_hosts.append(ad_host)
+        a_host()
+
+# printing hosts
+def p_hosts():
+    for i in a_hosts:
+        print(i)
+
+# pinging hosts
+def st_script():
+    while True:
+        for i in a_hosts:
+            p_hosts = ping(i)
+            if p_hosts.is_alive is True:
+                print(p_hosts.address + " is up")
+            else:
+                print(p_hosts.address + " is down")
+
+
+start_page()
